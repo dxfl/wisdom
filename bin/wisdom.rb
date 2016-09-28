8f45e3a04f5e2dabd79bd027defc5f5c078c133b
dix <thedixie@gmail.com>
2016-09-28 18:30:08 -0400

    add program wisdom

---
+++ bin/wisdom.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+# process arxiv downloaded pdf files
+
+require_relative '../lib/pdf_interface'
+require_relative '../lib/mongo_interface'
+require 'logger'
+require 'optparse'
+
+options = {}
+option_parse = OptionParser.new do |opts|
+  executable_name = File.basename($PROGRAM_NAME)
+  opts.banner = <<-EOF
+Extract papers from downloaded arxiv pdfs and send them to mongo
+
+Usage: #{executable_name} [options]
+
+EOF
+
+  #directory
+  opts.on("-d", "--directory DIRECTORY", 'indicate main pdf directory') do |dir|
+    options[:directory] = dir
+  end
+end
+
+option_parse.parse!
+
+MAIN_DIR = options[:directory]
+
+LOG = Logger.new($stderr)
+LOG.level = Logger.const_get(ENV.fetch("LOG_LEVEL", "INFO"))
+
+def delete_dots list
+  list.delete(".")
+  list.delete("..")
+  list
+end
+
+def get_directories directory
+  dir_list = delete_dots Dir.entries(directory)
+  #dir_list.select{ |dir| Dir.exist?(dir)}
+end
+
+def get_files folder
+  delete_dots Dir.entries(folder)
+end
+
+def process
+  mongo = MongoInterface.new("arxiv", "test")
+  pdf_directories = get_directories MAIN_DIR
+  pdf_directories.each do |dir|
+    path = MAIN_DIR + dir + "/"
+    pdf_files = get_files path
+    pdf_files.each do |file|
+      pdf = PDFInterface.new(path + file)
+      pdf.process
+      posts = [{filename: file, #pdf.filename includes path
+                title: pdf.title,
+                authors: pdf.authors,
+                abstract: pdf.abstract,
+                body: pdf.body}]
+      mongo.save posts
+      LOG.info "Processed paper: #{pdf.filename}"
+    end
+  end
+end
+
+
+process
+
+
+
